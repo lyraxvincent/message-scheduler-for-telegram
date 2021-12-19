9209e06a70f690e6b66e12d466a10dae72a1d6af
lyraxvincent <njongevincent@gmail.com>
2021-12-19 23:33:58 +0300

    first push

---
+++ deftime_bot.py
@@ -0,0 +1,75 @@
+import os
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, messagehandler
+import logging
+import schedule
+
+# Auth token
+TOKEN = open("authkey", 'r').readline() #str(os.environ['TOKEN_KEY'])
+
+updater = Updater(token=TOKEN)
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+
+# =========================================================================================================
+# Non-API functions
+# =========================================================================================================
+def get_message(text):
+    text_lower = text.lower()
+    first_index = text_lower.split().index('send')
+    second_index = text_lower.split().index('to')
+    msg = " ".join([word.strip('"').strip("'") for word in text.split()[first_index+1:second_index]])
+    return msg
+
+#def get_user(text):
+
+
+def get_send_time(text):
+    text_lower = text.lower()
+    time_index = text_lower.split().index('at') + 1
+    tm = text.split()[time_index]
+    return tm
+
+
+
+# =========================================================================================================
+
+
+# A function to process a specific type of update:
+def start(update, context):
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text="""Hi! I'm a bot designed to help you schedule telegram messages.\n
+        To send a message at time '12:34' to user 'John', tell me to do so in this form:\n\n 
+        send 'I love you!' to John at 12:34\n\nTalk to me!""")
+
+# A function called every time the Bot receives a Telegram message that contains the /start command
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+
+
+
+def schedule_message(update, context):
+    # update.message.text
+    msg = get_message(update.message.text)
+    tm = get_send_time(update.message.text)
+    usr = "@lyraxvincent"
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+# A function called every time the Bot receives a Telegram message that contains the /schedule_message command
+scheduleMessage_handler = CommandHandler('schedule_message', schedule_message)
+dispatcher.add_handler(scheduleMessage_handler)
+
+
+# For commands that the bot doesn't understand:
+def unknown(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
+
+unknown_handler = MessageHandler(Filters.command, unknown)
+dispatcher.add_handler(unknown_handler)
+
+
+
+# starting the bot
+updater.start_polling()
