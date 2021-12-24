3129344153db85fe231610c55e1d8bb6c1d6bd37
lyraxvincent <njongevincent@gmail.com>
2021-12-24 23:21:21 +0300

    pending ValueError: 'send' is not in list

--- deftime_bot.py
+++ deftime_bot.py
@@ -1,4 +1,5 @@
 import os
+import telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, messagehandler
 import logging
 import schedule
@@ -6,11 +7,14 @@ import schedule
 # Auth token
 TOKEN = open("authkey", 'r').readline() #str(os.environ['TOKEN_KEY'])
 
+bot = telegram.Bot(token=TOKEN)
+#updates = bot.get_updates()
+#print(updates[0]['message']['forward_from']['id']); exit()
+
 updater = Updater(token=TOKEN)
 dispatcher = updater.dispatcher
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
 
 # =========================================================================================================
 # Non-API functions
@@ -51,11 +55,17 @@ dispatcher.add_handler(start_handler)
 
 
 def schedule_message(update, context):
-    # update.message.text
+    # Grab required text elements
     msg = get_message(update.message.text)
     tm = get_send_time(update.message.text)
-    usr = "@lyraxvincent"
-    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+    # update of the person to send message to
+    # we forward any message from this user to the bot first so that we can get user info for the bot
+    usr = bot.get_updates()[0]['message']['forward_from']['id']
+    #usr = 404768059
+    #context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    context.bot.send_message(chat_id=usr, text=msg)
+    #bot.sendMessage(chat_id=usr, text=msg)
 
 # A function called every time the Bot receives a Telegram message that contains the /schedule_message command
 scheduleMessage_handler = CommandHandler('schedule_message', schedule_message)
